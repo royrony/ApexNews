a6b65eee47d0cb9f098de5ac0682a6ad2d9af290
royrony <ronyroy222@gmail.com>
2018-03-26 09:37:47 +0530

    initial commits

--- newsapp/models.py
+++ newsapp/models.py
@@ -82,4 +82,4 @@ class Comment(models.Model):
         ordering = ('created',)
 
     def __str__(self):
-        return 'Comment by {} on {}'.format(self.name, self.post)
+        return 'Comment by {} on {}'.format(self.name, self.article)
